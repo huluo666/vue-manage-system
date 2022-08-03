a031b0a0b6dde434e005d2a53900103624c03fde
jenkins <huluo@leniu.com>
2022-08-03 14:55:49 +0800

    升级依赖库，解决警告You are running the esm-bundler build of vue-i18n.

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ export default {
     plugins: [vue()],
     optimizeDeps: {
         include: ['schart.js']
+    },
+    define: {
+        __VUE_I18N_FULL_INSTALL__: true,
+        __VUE_I18N_LEGACY_API__: false,
+        __INTLIFY_PROD_DEVTOOLS__: false,
     }
 }
